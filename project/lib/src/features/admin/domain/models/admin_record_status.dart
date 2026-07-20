1cdbc90f7841ae4f6806dd6c3684adb1ccdd5b37
Nguyễn Vĩnh Giang <163664858+giangnv2210@users.noreply.github.com>
2026-07-20 11:31:56 +0700

    lab

--- project/lib/src/features/admin/domain/models/admin_record_status.dart
+++
@@ -1,8 +0,0 @@
-enum AdminRecordStatus {
-  active('Active'),
-  inactive('Inactive');
-
-  const AdminRecordStatus(this.label);
-
-  final String label;
-}
--- project/lib/src/features/auth/auth.dart
+++
@@ -1,3 +0,0 @@
-export 'presentation/screens/login_screen.dart';
-export 'presentation/screens/register_screen.dart';
-export 'presentation/screens/reset_password_screen.dart';
